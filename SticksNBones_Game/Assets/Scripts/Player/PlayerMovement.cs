6cdb7178003cec7ac7f7188773728ef872a71289
Adrian <skatehumor@yahoo.com>
2018-06-09 01:32:26 -0400

    Preliminary movement animations and controls setup

--- SticksNBones_Game/Assets/Scripts/Player/PlayerMovement.cs
+++ SticksNBones_Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,47 +6,114 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] float dashSpeed = 9.0f;
+    [SerializeField] float skipSpeed = 1.23f;
     [SerializeField] float jumpVelocity = 12.0f;
     [SerializeField] PlayerRole role = PlayerRole.Local;
 
     private Animator playerAnimator;
-    private bool grounded = true;
 
-	// Use this for initialization
 	void Start () {
         playerAnimator = GetComponent<Animator>();
+
+        if (role == PlayerRole.Local) {
+            SNBGlobal.thisPlayer.state.OnComboEvent += HandleComboEvent;
+        }
 	}
 	
-	// Update is called once per frame
 	void Update () {
         if (role == PlayerRole.Local) {
-            MoveCharacter();
+            RespondToHAxis();
             CharacterJump();
+            LookAtOpponent();
+        }
+    }
+
+    private void LookAtOpponent() {
+        PlayerMovement[] players = GameObject.FindObjectsOfType<PlayerMovement>();
+        foreach (PlayerMovement p in players) {
+            if (p.gameObject != this.gameObject) {
+                Vector3 dist = p.gameObject.transform.position - transform.position;
+                transform.rotation = dist.x < 0 ? Quaternion.Euler(0, -90f, 0) : Quaternion.Euler(0, 90f, 0);   // warning: 90f might act as magic number
+            }
         }
     }
 
     private void CharacterJump() {
-        if (Input.GetButtonDown("Jump") && grounded) {
+        if (Input.GetButtonDown("Jump") && SNBGlobal.thisPlayer.state.grounded) {
             GetComponent<Rigidbody>().velocity = new Vector3(0, jumpVelocity);
             playerAnimator.SetTrigger("jump");
-            grounded = false;
+            SNBGlobal.thisPlayer.state.grounded = false;
         }
     }
 
-    private void MoveCharacter() {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        if (horizontalThrow != 0) {
-            float newX = transform.position.x + horizontalThrow * dashSpeed * Time.deltaTime;
-            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-            playerAnimator.SetBool("dashing", true);
+    private void RespondToHAxis() {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        SNBGlobal.thisPlayer.state.lastHorizontal = horizontalMove;
+        Move();
+    }
+
+    private void Move() {
+        if (SNBGlobal.thisPlayer.state.lastHorizontal != 0) {
+            if (!SNBGlobal.thisPlayer.state.dashing && !SNBGlobal.thisPlayer.state.skipping) {
+                SNBGlobal.thisPlayer.ExecuteMove(BasicMove.Move);
+            }
+
+            if (SNBGlobal.thisPlayer.state.dashing) {
+                Dash();
+            } else {
+                Skip();
+            }
         } else {
-            playerAnimator.SetBool("dashing", false);
+            StopDash();
+            StopSkip();
+        }
+    }
+
+    private void Dash() {
+        float newX = transform.position.x + SNBGlobal.thisPlayer.state.lastHorizontal * dashSpeed * Time.deltaTime;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        if (!SNBGlobal.thisPlayer.state.dashing) {
+            SNBGlobal.thisPlayer.state.dashing = true;
+            playerAnimator.Play("Dash", 0, 0f);
+        }
+    }
+
+    private void Skip() {
+        float newX = transform.position.x + SNBGlobal.thisPlayer.state.lastHorizontal * skipSpeed * Time.deltaTime;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        if (!SNBGlobal.thisPlayer.state.skipping) {
+            SNBGlobal.thisPlayer.state.skipping = true;
+            playerAnimator.Play("Skip", 0, 0f);
+        }
+    }
+
+    private void StopDash() {
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) {
+            playerAnimator.Play("Idle", 0, 0f);
+        }
+        SNBGlobal.thisPlayer.state.dashing = false;
+    }
+
+    private void StopSkip() {
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) {
+            playerAnimator.Play("Idle", 0, 0f);
+        }
+        SNBGlobal.thisPlayer.state.skipping = false;
+    }
+
+    private void HandleComboEvent(ComboType combo) {
+        switch (combo) {
+            case ComboType.Dash:
+                Dash();
+                break;
+            default:
+                break;
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")) {
-            grounded = true;
+            SNBGlobal.thisPlayer.state.grounded = true;
         }
     }
 }
--- SticksNBones_Game/Assets/Scripts/SNBGlobal.cs
+++ SticksNBones_Game/Assets/Scripts/SNBGlobal.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Timers;
+
+using UnityEngine;
 
 public enum CharacterType { Classico, Ranger, None };
 public enum PlayerStatus { NotReady, Ready, Dead /* etc. */ };
-public enum PlayerRole { Local, Opponent, Sandbag, Bot }
+public enum PlayerRole { Local, Opponent, Sandbag, Bot };
+public enum BasicMove { AirKick, AirPunch, Move, MoveBack,
+                       MovingJump, StaticJump, Punch,
+                       Kick };
+public enum ComboType { Dash, DashBack };
+
+public struct MoveInfo {
+    public BasicMove move;
+    public int sequenceNumber;
+    public double sequenceTime;
+    public string moveKey;
+
+    public MoveInfo(BasicMove m, int seqNum, double seqTime, string mk = null) {
+        move = m;
+        sequenceNumber = seqNum;
+        sequenceTime = seqTime;
+        moveKey = mk;
+    }
+}
 
 public static class SNBGlobal : object {
     public static readonly string defaultServerIP = "127.0.0.1";
     public static readonly int defaultServerPort = 50777;
     public static readonly int maxBufferSize = 2048;
     public static readonly int defaultMatchPort = 60000;
+    public static readonly double comboDeltaTime = 400;
+
+    public static readonly Dictionary<ComboType, List<BasicMove>> combos = new Dictionary<ComboType, List<BasicMove>>() {
+        { ComboType.Dash, new List<BasicMove>() { BasicMove.Move, BasicMove.Move } },
+        { ComboType.DashBack, new List<BasicMove>() { BasicMove.MoveBack, BasicMove.MoveBack } }
+    };
     
     private static readonly string[] usernameAdjectives = {"Growling", "Floating", "Mean", "Arcadian", "Friable", "Noxious", "Luminous", "Turbulent", "Nebulous", "Arc"};
     private static readonly string[] usernameNouns = {"Ghost", "Glass", "Connection", "Pump", "Hill", "Cactus", "Nation", "Flavor", "Metal", "Spring"};
 
-    private static Random rnd = new Random();
+    private static System.Random rnd = new System.Random();
 
     public static SNBPlayer thisPlayer = new SNBPlayer();
 
@@ -38,16 +65,88 @@ public class SNBPlayer {
         status = PlayerStatus.NotReady;
         state = new SNBPlayerState();
     }
+
+    public void ExecuteMove(BasicMove move) {
+        if (!state.inCombo) state.StartCombo();
+        state.AddToCombo(move);
+    }
 }
 
 public class SNBPlayerState {
+    public delegate void ComboEvent(ComboType combo);
+    public event ComboEvent OnComboEvent;
+
+    public bool grounded = true;
     public bool dashing = false;
     public bool skipping = false;
     public bool blocking = false;
-    public float lastHorizontalThrow = 0f;
-    // todo: last combo array
+    public float lastHorizontal = 0f;
+
+    private Timer comboTimer = new Timer();
+    public double elapsedComboTime = 0;
+    public List<MoveInfo> currentCombo = new List<MoveInfo>();
+
+    public bool inCombo { get { return currentCombo.Count > 0; } }
+    public bool idle { get { return !skipping && !dashing;  } }
+
+    public SNBPlayerState() {
+        comboTimer.Elapsed += (sender, eventArgs) => ComboElapsedHandler();
+        comboTimer.Enabled = false;
+    }
+
+    private void ComboElapsedHandler() {
+        elapsedComboTime += comboTimer.Interval;
+
+        if (currentCombo.Count > 0 && 
+            (elapsedComboTime - currentCombo[currentCombo.Count - 1].sequenceTime) > SNBGlobal.comboDeltaTime) {
+            EndCombo();
+        }
+    }
+
+    public void StartCombo() {
+        comboTimer.Start();
+    }
+
+    public void AddToCombo(BasicMove move) {
+        MoveInfo m = new MoveInfo(move, currentCombo.Count + 1, elapsedComboTime);
+        currentCombo.Add(m);
+
+        if (inCombo) {
+            CheckCombo(currentCombo);
+        }
+    }
+
+    private void CheckCombo(List<MoveInfo> currentCombo) {
+        foreach (ComboType ct in SNBGlobal.combos.Keys) {
+            if (ComboSequencesMatch(SNBGlobal.combos[ct], currentCombo)) {
+                OnComboEvent(ct); return;
+            }
+        }
+    }
+
+    private bool ComboSequencesMatch(List<BasicMove> comboSequence, List<MoveInfo> moves) {
+        if (comboSequence.Count != moves.Count) return false;
+        for (int i = 0; i < comboSequence.Count; i++) {
+            if (comboSequence[i] != moves[i].move) return false;
+        }
+        return true;
+    }
+
+    public void EndCombo() {
+        comboTimer.Stop();
+        currentCombo.Clear();
+        elapsedComboTime = 0;
+    }
 
     public string ToJson() {
-        return "{}";
+        return "{\"dashing\": " + dashing + ", " +
+                "\"skipping\": " + skipping + "," +
+                "\"blocking\": " + blocking + "," +
+                "\"lastHorizontalThrow\": " + lastHorizontal + "}";
+    }
+
+    public static SNBPlayerState FromJson() {
+        // todo
+        return new SNBPlayerState();
     }
 }
